8df206fb2b4a4d78e8a2d88c171bfdd0b290cc4c
Tim Akintsev <alerion107@yandex.ru>
2022-04-05 00:10:08 +0500

    исправления после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,9 @@ let popupWindow = document.querySelector('.popup');
 let popupCloseBtn = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__title');
 let profileActivity = document.querySelector('.profile__subtitle');
-let popupName = document.querySelector('.popup__input_name');
-let popupActivity = document.querySelector('.popup__input_activity');
-let popupSubmit = document.querySelector('.popup__submit-button');
+let popupName = document.querySelector('.popup__input_type_name');
+let popupActivity = document.querySelector('.popup__input_type_activity');
+let popupForm = document.querySelector('.popup__form');
 
 function openPopupWindow() {
   popupWindow.classList.add('popup_is-opened');
@@ -17,16 +17,11 @@ function closePopupWindow(){
   popupWindow.classList.remove('popup_is-opened');
 }
 
-editButton.addEventListener('click', openPopupWindow);
-popupCloseBtn.addEventListener('click', closePopupWindow);
-
 function onOverlayClick(event) {
   if (event.target===event.currentTarget) {
     closePopupWindow();
   }
 }
-
-popupWindow.addEventListener('click', onOverlayClick);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -35,4 +30,7 @@ function formSubmitHandler(evt) {
   closePopupWindow();
 }
 
-popupSubmit.addEventListener('click', formSubmitHandler);
+editButton.addEventListener('click', openPopupWindow);
+popupCloseBtn.addEventListener('click', closePopupWindow);
+popupWindow.addEventListener('click', onOverlayClick);
+popupForm.addEventListener('submit', formSubmitHandler);
